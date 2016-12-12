593aa9e882c259685053683b4f0821f9dd579727
DeeAnn Uhlarik <duhlarik@pillartechnology.com>
2016-12-12 17:02:23 -0500

    refactored to remove one helper method

--- src/main/java/roman/RomanToNumberToRoman.java
+++ src/main/java/roman/RomanToNumberToRoman.java
@@ -10,34 +10,23 @@ public class RomanToNumberToRoman {
 		int intValueOfRoman = 0;
 		char currentRoman;
 		int valueOfCurrentRoman = 0;
-		int valueOfRomanToTheLeft = 0;
+		int valueOfPreviousRoman = 0;
 		int i = roman.length() - 1;
 		while (i >= 0) {
 			currentRoman = roman.charAt(i);
 			valueOfCurrentRoman = getIntValueOfCurrentRoman(currentRoman);
-			valueOfRomanToTheLeft = getIntValueOfRomanToTheLeft(roman, valueOfRomanToTheLeft, i);
-				if (valueOfRomanToTheLeft > 0 && valueOfRomanToTheLeft < valueOfCurrentRoman) {
-					valueOfRomanToTheLeft *= -1;
-					valueOfCurrentRoman += valueOfRomanToTheLeft;
-					i--;
-				}
+			if (valueOfCurrentRoman > 0 && valueOfCurrentRoman < valueOfPreviousRoman) {
+				valueOfCurrentRoman *= -1;
+			}
 			intValueOfRoman += valueOfCurrentRoman;
+			valueOfPreviousRoman = valueOfCurrentRoman;
 			i--;
 		}
 		return intValueOfRoman;
 	}
 
-	private int getIntValueOfRomanToTheLeft(String roman, int valueOfRomanToTheLeft, int i) {
-		char romanToTheLeft;
-		if(i > 0) {
-			romanToTheLeft = roman.charAt(i-1);
-			valueOfRomanToTheLeft = getIntValueOfCurrentRoman(romanToTheLeft);
-		}
-		return valueOfRomanToTheLeft;
-	}
-
 	private int getIntValueOfCurrentRoman(char currentRoman) {
-		int currentNumber= -1;
+		int currentNumber = -1;
 		for (int j = 0; j < romans.length; j++) {
 			if (currentRoman == romans[j]) {
 				currentNumber = numbers[j];
