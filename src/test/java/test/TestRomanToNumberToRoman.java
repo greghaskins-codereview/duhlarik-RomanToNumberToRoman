b16c8785c27150e5d79eab2598621641ec5594b2
DeeAnn Uhlarik <duhlarik@pillartechnology.com>
2016-12-13 17:22:17 -0500

    started to add code to convert numbers to numerals

--- src/test/java/test/TestRomanToNumberToRoman.java
+++ src/test/java/test/TestRomanToNumberToRoman.java
@@ -6,75 +6,108 @@ import org.junit.Test;
 import roman.RomanToNumberToRoman;
 
 public class TestRomanToNumberToRoman {
-	RomanToNumberToRoman roman;
+	RomanToNumberToRoman input;
 
 	@Before
 	public void setUp() {
-	roman = new RomanToNumberToRoman();
+		input = new RomanToNumberToRoman();
 	}
 
 	@Test
-	public void romanItoNumber1() {
-		assertEquals(1, roman.toNumber("I"));
-	}
-
-	@Test
-	public void romanVtoNumber5() {
-		assertEquals(5, roman.toNumber("V"));
-	}
-
-	@Test
-	public void romanXtoNumber10() {
-		assertEquals(10, roman.toNumber("X"));
+	public void aSingleRomanItoANumber() {
+		assertEquals(1, input.toNumber("I"));
+		assertEquals(5, input.toNumber("V"));
+		assertEquals(10, input.toNumber("X"));
 	}
 
 	@Test
 	public void twoOfTheSameRomanNumeralsToANumber() {
-		assertEquals(2, roman.toNumber("II"));
+		assertEquals(2, input.toNumber("II"));
+		assertEquals(20, input.toNumber("XX"));
 	}
 
 	@Test
 	public void twoDifferentRomanNumeralsAddedTogether() {
-		assertEquals(6, roman.toNumber("VI"));
+		assertEquals(6, input.toNumber("VI"));
+		assertEquals(11, input.toNumber("XI"));
 	}
 
 	@Test
 	public void twoDifferentRomanNumeralsSubtractedToGetNumber() {
-		assertEquals(4, roman.toNumber("IV"));
+		assertEquals(4, input.toNumber("IV"));
+		assertEquals(9, input.toNumber("IX"));
 	}
 
 	@Test
-	public void threeOfTheSameRomanNumeraltoaNumber_IIIto3() {
-		assertEquals(3, roman.toNumber("III"));
+	public void threeOfTheSameRomanNumeraltoaNumber() {
+		assertEquals(3, input.toNumber("III"));
+		assertEquals(30, input.toNumber("XXX"));
 	}
 
-	@Test
-	public void threeOfTheSameRomanNumeraltoaNumber_XXXto30() {
-		assertEquals(30, roman.toNumber("XXX"));
-	}
-	
 	@Test
 	public void aLargeRomanNumeraltoaNumberWithoutSubtraction() {
-		assertEquals(77, roman.toNumber("LXXVII"));
+		assertEquals(77, input.toNumber("LXXVII"));
+		assertEquals(3671, input.toNumber("MMMDCLXVVI"));
 	}
-	
+
 	@Test
 	public void aLargeRomanNumeraltoaNumberWithSubtraction() {
-		assertEquals(74, roman.toNumber("LXXIV"));
+		assertEquals(74, input.toNumber("LXXIV"));
+		assertEquals(439, input.toNumber("CDXXXIX"));
+		assertEquals(3934, input.toNumber("MMMCMXXXIV"));
+	}
+
+	@Test
+	public void theNumber1ToASingleRomanI() {
+		assertEquals("I", input.toRoman(1));
+	}
+
+	@Test
+	public void theNumber2ToTwoOfTheSameRomanNumeral_II() {
+		assertEquals("II", input.toRoman(2));
+	}
+
+	@Test
+	public void theNumber3ToThreeOfTheSameRomanNumeral_III() {
+		assertEquals("III", input.toRoman(3));
+	}
+	@Test
+	public void theNumber4ToARomanNumeralWithSubtraction_IV() {
+		assertEquals("IV", input.toRoman(4));
 	}
 	
 	@Test
-	public void aLargeRomanNumeralWithThreeOfTheSameNumeralWithSubtraction() {
-		assertEquals(39, roman.toNumber("XXXIX"));
+	public void theNumber5ToASingleRomanV() {
+		assertEquals("V", input.toRoman(5));
 	}
-	
+
 	@Test
-	public void aVeryLargeRomanNumeralWithManyOfTheSameNumeralWithSubtraction() {
-		assertEquals(3934, roman.toNumber("MMMCMXXXIV"));
+	public void theNumber6ToARomanWithAddition_VI() {
+		assertEquals("VI", input.toRoman(6));
 	}
-	
+
 	@Test
-	public void aVeryLargeRomanNumeralWithThreeOfTheSameNumeralWithoutSubtraction() {
-		assertEquals(3000, roman.toNumber("MMM"));
+	public void theNumber7ToARomanWithAddition_VII() {
+		assertEquals("VII", input.toRoman(7));
+	}
+
+	@Test
+	public void theNumber8ToARomanWithAddition_VIII() {
+		assertEquals("VIII", input.toRoman(8));
+	}
+
+	@Test
+	public void theNumber9ToARomanWithSubtraction_IX() {
+		assertEquals("IX", input.toRoman(9));
+	}
+
+	@Test
+	public void theNumber10ToASingleRoman_X() {
+		assertEquals("X", input.toRoman(10));
+	}
+
+	@Test
+	public void theNumber11ToARomanWithAddition_XI() {
+		assertEquals("XI", input.toRoman(11));
 	}
 }
